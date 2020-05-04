de07e111ccd8e4b157c57a1b1e79fe25b00dff08
anilgit627 <mailto:srisasanka096@gmail.com>
2020-05-04 15:31:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const forSSL = function() {
+    return function(req, res, next) {
+        if (req.headers['x-forwardred-photo'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('host'), req.url].join(''));
+        }
+        next();
+    }
+}
+app.use(express.static(__dirname + '/dist/HomeWebsite'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/HomeWebsite/index.html'))
+})
+app.listen(process.env.PORT || 8081)
