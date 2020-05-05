76995553899fc3d2d01f4c31de13d18ab80d8ff0
anilgit627 <mailto:srisasanka096@gmail.com>
2020-05-05 19:54:37 +0530

    gallery content added and small changes in caraual

--- src/app/components/app-body/app-body.component.ts
+++ src/app/components/app-body/app-body.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-app-body',
@@ -10,6 +12,27 @@ export class AppBodyComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready( () => {
+      // initialize swiper when document ready
+      // tslint:disable-next-line: prefer-const
+       var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows : true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    });
   }
 
 }
