74d44f89dd49ec54bf09d24ddc12f9bbe82d1a38
anilgit627 <mailto:srisasanka096@gmail.com>
2020-09-07 15:20:38 +0530

    gif added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'HomeWebsite';
+  ngOnInit() {
+    $('#loading_indicator').show();
+  }
+  ngAfterViewInit() {
+    $('#loading_indicator').hide();
+  }
 }
